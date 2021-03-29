b7ff47e993ec837610421a4722946a8f38a885e0
Ueveys Caparlar <geschaeftlich82@gmail.com>
2021-03-29 09:32:31 +0200

    umbenannt der Variabelname

--- JSFiles/pageObjects/LampenweltHomepage.js
+++ JSFiles/pageObjects/LampenweltHomepage.js
@@ -5,8 +5,6 @@ const protractor_1 = require("protractor");
 class LampenweltHomepage {
     constructor() {
         this.nav1 = protractor_1.element(protractor_1.by.xpath("//li[@class='level0 nav-2 parent' and position()=1]/*/span"));
-        //aussenleuchten = browser.driver.findElement(by.xpath("//li[@class='level0 nav-2 parent' and position()=2]"));
-        //smartHome = browser.driver.findElement(by.xpath("//li[@class='level0 nav-2 parent' and position()=3]"));
         this.kateTitel = protractor_1.element(protractor_1.by.xpath("//h1[@class='category-title__headline']"));
     }
     urlAufruf() {
@@ -14,4 +12,4 @@ class LampenweltHomepage {
     }
 }
 exports.LampenweltHomepage = LampenweltHomepage;
-//# sourceMappingURL=data:application/json;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoiTGFtcGVud2VsdEhvbWVwYWdlLmpzIiwic291cmNlUm9vdCI6IiIsInNvdXJjZXMiOlsiLi4vLi4vcGFnZU9iamVjdHMvTGFtcGVud2VsdEhvbWVwYWdlLnRzIl0sIm5hbWVzIjpbXSwibWFwcGluZ3MiOiI7OztBQUFBLDJDQUFnRDtBQUVoRCxNQUFhLGtCQUFrQjtJQUEvQjtRQUNFLFNBQUksR0FBRyxvQkFBTyxDQUFDLGVBQUUsQ0FBQyxLQUFLLENBQUMsNERBQTRELENBQUMsQ0FBQyxDQUFDO1FBQ3ZGLCtHQUErRztRQUMvRywwR0FBMEc7UUFFMUcsY0FBUyxHQUFHLG9CQUFPLENBQUMsZUFBRSxDQUFDLEtBQUssQ0FBQyx5Q0FBeUMsQ0FBQyxDQUFDLENBQUM7SUFNM0UsQ0FBQztJQUpDLFNBQVM7UUFDUCxvQkFBTyxDQUFDLEdBQUcsQ0FBQyw0QkFBNEIsQ0FBQyxDQUFDO0lBQzVDLENBQUM7Q0FFRjtBQVhELGdEQVdDIn0=
+//# sourceMappingURL=data:application/json;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoiTGFtcGVud2VsdEhvbWVwYWdlLmpzIiwic291cmNlUm9vdCI6IiIsInNvdXJjZXMiOlsiLi4vLi4vcGFnZU9iamVjdHMvTGFtcGVud2VsdEhvbWVwYWdlLnRzIl0sIm5hbWVzIjpbXSwibWFwcGluZ3MiOiI7OztBQUFBLDJDQUFnRDtBQUVoRCxNQUFhLGtCQUFrQjtJQUEvQjtRQUNFLFNBQUksR0FBRyxvQkFBTyxDQUFDLGVBQUUsQ0FBQyxLQUFLLENBQUMsNERBQTRELENBQUMsQ0FBQyxDQUFDO1FBQ3ZGLGNBQVMsR0FBRyxvQkFBTyxDQUFDLGVBQUUsQ0FBQyxLQUFLLENBQUMseUNBQXlDLENBQUMsQ0FBQyxDQUFDO0lBTTNFLENBQUM7SUFKQyxTQUFTO1FBQ1Asb0JBQU8sQ0FBQyxHQUFHLENBQUMsNEJBQTRCLENBQUMsQ0FBQztJQUM1QyxDQUFDO0NBRUY7QUFSRCxnREFRQyJ9
