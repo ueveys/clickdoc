85f6db88cb2d6eff406d45065b3d53aba1f04a4e
ueveys <80990954+ueveys@users.noreply.github.com>
2021-04-07 17:32:39 +0200

    Update clickdoc_loginPopup_page.ts
    lokator wurde angepasst.

--- pageObjects/clickdoc_loginPopup_page.ts
+++ pageObjects/clickdoc_loginPopup_page.ts
@@ -8,8 +8,8 @@ export class LoginPopup {
   public static inputPassword = element(by.css("#mat-input-2"));
   public static btnPasswordForgot = element(by.xpath("//a[@data-web-test='login_forgot_password']"));
   public static btnRegister = element(by.xpath("//button[@data-web-test='login_register_btn']"));
-  //btnLogin = element(by.xpath("//button[@class='life-primary-btn']"));
-  public static btnLogin = element(by.css(".d-none > .life-primary-btn"));
+  public static btnLogin = element(by.xpath("//button[@class='life-primary-btn']"));
+  //public static btnLogin = element(by.css(".d-none > .life-primary-btn"));
   public static hinweisFalschePassword = element(by.xpath("//app-error-message//p[@class='mt-0']"));
   public static hinweismeldungEmail = element(by.css(".error-container"));
   public static btnJetztRegistirieren = element(by.xpath("//button[@class='life-primary-btn' and contains(text(),'Jetzt registrieren')]"));
